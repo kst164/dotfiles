da124be14cae9c36783d2ead5d28c108322b3984
kst164 <kartheek@tammana.org>
2022-01-13 10:57:58 +0530

    stuff

--- zsh/custom/aliases.zsh
+++ zsh/custom/aliases.zsh
@@ -6,10 +6,17 @@ hsim() {
   sh ~/code/courses/nand2tetris/tools/HardwareSimulator.sh $1
 }
 
+# college vpn
 wgup() {
   wg-quick up wg0
 }
 
 wgdown() {
   wg-quick down wg0
+}
+
+# get myself to stop using vi, use vim or nvim instead
+vi() {
+  echo "NO"
+  return 1
 }
